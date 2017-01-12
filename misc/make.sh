dc60db695dc13555390da151cecf11ce5da1d5ec
culdev <culdev@users.noreply.github.com>
2017-01-12 07:46:17 +0100

    Download and untar phantomjs

--- misc/make.sh
+++ misc/make.sh
@@ -11,11 +11,16 @@ export LIB_DIR=${ROOT_DIR}/lib
 export CONF_DIR=${OPENSHIFT_REPO_DIR}/conf
 
 export DIST_PHP_VER=5.6.28
+export DIST_PHANTOMJS_VER=2.1.1
 
 pushd ${OPENSHIFT_REPO_DIR}/misc
 
 chmod +x make_php
 source make_php
 check_php
+
+chmod +x get_phantomjs
+source get_phantomjs
+check_phantomjs
 
 popd
